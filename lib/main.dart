64c5a8bc4251de47c9618d57dcf3536dc34a80ea
Otabek Nabijonov <nabijonovo23@gmail.com>
2024-03-08 20:42:17 +0300

    project updated

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:todo_app/data/model/todo_model.dart';
 import 'package:todo_app/presentation/screen/splash/splash_screen.dart';
 import 'package:todo_app/utils/constants.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
